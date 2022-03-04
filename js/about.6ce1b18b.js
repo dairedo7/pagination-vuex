0e9dfb9c2606b76236a487fe8e5c2334ef0d1c8a
Vladyslav Svitana <vladik719@gmail.com>
2022-03-04 20:30:52 +0100

    New Deployment

---
+++ js/about.6ce1b18b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This assignment has been carried out for learning purposes only.")])])}],r=t("2877"),u={},i=Object(r["a"])(u,s,a,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=about.6ce1b18b.js.map
